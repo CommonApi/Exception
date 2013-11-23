28ad30ec17ef62c90a86b87aa5ee41d37828c435
Amy Stephen <AmyStephen@gmail.com>
2013-11-23 00:20:36 -0600

    Exception Library

---
+++ ExceptionInterface.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Exception Interface
+ *
+ * @package    Http
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright  2013 Common Api. All rights reserved.
+ */
+namespace CommonApi\Exception;
+
+/**
+ * Exception Interface
+ *
+ * @package    Exception
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright  2013 Common Api. All rights reserved.
+ * @since      1.0
+ */
+interface ExceptionInterface
+{
+    public function __toString();
+}
